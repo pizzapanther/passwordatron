5e20021a8bbac6aad61e43cdbe4a852c64843a03
Paul Bailey <paul@neutron.studio>
2024-11-09 17:27:46 -0600

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# passwordatron
-Insecure way to secure your applications
+# Passwordatron
+
+*Insecure way to secure your applications*
+
+A small utility that requires a password before starting an application. Let's you limit access to applications on a shared computer. Note, anyone with some tech skills will find a way around this. Think of it as a door to let people know not use that application.
+
+## Configuration File
+
+```json
+{
+  "password": "super-insecure-way-of-storing-a-password",
+  "app-name1": "/path/to/application add-some-args --like-this",
+  "app-name2": "/path/to/another/application add-some-args --like-this"
+}
+```
---
+++ passwordatron.py
@@ -0,0 +1,57 @@
+import json
+import sys
+import os
+
+from pathlib import Path
+
+import tkinter as tk
+from tkinter import font
+from tkinter import ttk
+
+class App(tk.Frame):
+  def __init__(self, start_app, password=None, **apps):
+    self.start_app = start_app
+    self.password = password
+    self.apps = apps
+    self.root = tk.Tk()
+    self.defaultFont = font.nametofont("TkDefaultFont")
+    self.defaultFont.configure(size=14)
+
+    super().__init__(self.root)
+    self.pack()
+
+    ttk.Label(self, text="Enter Password").pack(pady=10, padx=10, side="top")
+
+    self.pw = tk.Entry(self, show="*")
+    self.pw.pack(pady=10, padx=10, side="top")
+
+    self.contents = tk.StringVar()
+    self.pw["textvariable"] = self.contents
+    self.pw.bind('<Key-Return>', self.pw_contents)
+    self.pw.focus()
+
+    bframe = tk.Frame(self)
+    bframe.pack(side="top")
+    ttk.Button(bframe, text="OK", command=self.pw_contents).pack(side="left", anchor="w", pady=10, padx=10)
+    ttk.Button(bframe, text="Quit", command=self.root.destroy).pack(side="left", anchor="w", pady=10, padx=10)
+
+  def pw_contents(self, *args):
+    if self.contents.get() == self.password:
+      self.root.destroy()
+      os.system(self.apps[self.start_app])
+
+    self.contents.set('')
+
+def run():
+  config_path = Path(__file__).parent / 'config.json'
+  with config_path.open('r') as fh:
+    options = json.load(fh)
+
+  pwapp = App(sys.argv[1], **options)
+  pwapp.master.title("Passwordatron")
+  pwapp.master.maxsize(400, 400)
+  pwapp.root.mainloop()
+
+
+if __name__ == "__main__":
+  run()
